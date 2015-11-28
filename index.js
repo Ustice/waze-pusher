9d30751afedbcb89cd4388c268d10c41a8370767
Ustice <ustice@gmail.com>
2015-11-28 01:40:44 -0500

    Updates code to es6

--- index.js
+++ index.js
@@ -1,61 +1,97 @@
-var _ = require('lodash');
-var app = require('express')();
-var request = require('superagent')
+'use strict';
 
-var server;
+let _ = require('lodash');
+let app = require('express')();
+let request = require('superagent');
+let Promise = require('bluebird');
+
+let agent = require('superagent-promise')(request, Promise);
+
+class ResponseError extends Error {
+    constructor(message, status) {
+        super(message);
+        this.name = this.constructor.name;
+        this.message = message;
+        if (typeof Error.captureStackTrace === 'function') {
+            Error.captureStackTrace(this, this.constructor.name);
+        } else {
+            this.stack = (new Error(message)).stack;
+        }
+
+        this.status = _.isUndefined(status) ? 0 : status;
+    }
+};
+
+class ServerError extends ResponseError {
+    constructor (message) {
+        super(message, 500);
+    }
+}
+
+class BadRequestError extends ResponseError {
+    constructor (message) {
+        super(message, 400);
+    }
+}
 
 app.get('/', function (req, res) {
-    if (req.query.address) {
+    let requiredAttributes = ['address'];
+
+    Promise.resolve(_.filter(requiredAttributes, attribute => _.isUndefined(req.query[attribute])))
+    .then(missingAttributes => {
+        if (missingAttributes.length) {
+            throw new BadRequestError('Missing attributes: ' + missingAttributes);
+        }
+    })
+    .then(() => agent.get('http://maps.google.com/maps/api/geocode/json?address=' + req.query.address + '&sensor=false&region=us'))
+    .catch(err => {
+        if (err.status === 400) {
+            throw new ServerError('Error getting lat-long.')
+        }
+
+        return err;
+    })
+    .then(response => {
+        let location = response.body.results[0].geometry.location;
+        let address = response.body.results[0].formatted_address;
+
+        let pushoverOptions = {
+            token: process.env.PUSHOVER_KEY,
+            user: process.env.PUSHOVER_USER,
+            message: 'Navigate to ' + address + '.',
+            device: 'iPhone',
+            title: 'Waze Navigation',
+            url: 'waze://?ll=' + location.lat + ',' + location.lng + '&navigate=yes',
+            url_title: 'Open in Waze'
+        };
+
         return request
-            .get('http://maps.google.com/maps/api/geocode/json?address=' + req.query.address + '&sensor=false&region=us')
-            .end(function (err, response) {
-                var address;
-                var location;
-                var pushoverOptions;
-                var pushoverBody;
-
-                if (err) {
-                    // Send to Pushover
-                    return res.status(500).send('Error getting lat-long.');
-                }
-
-                location = response.body.results[0].geometry.location;
-                address = response.body.results[0].formatted_address;
-
-                pushoverOptions = {
-                    token: process.env.PUSHOVER_KEY,
-                    user: process.env.PUSHOVER_USER,
-                    message: 'Navigate to ' + address + '.',
-                    device: 'iPhone',
-                    title: 'Waze Navigation',
-                    url: 'waze://?ll=' + location.lat + ',' + location.lng + '&navigate=yes',
-                    url_title: 'Open in Waze'
-                };
-
-
-
-                return request
-                    .post('https://api.pushover.net/1/messages.json')
-                    .type('form')
-                    .send(pushoverOptions)
-                    .end(function (err, pushoverResponse) {
-                        if (err) {
-                            return res.status(500).send('Error sending to Pushover.\n\n' + JSON.stringify(err, null, '  '));
-                        }
-
-                        return res.status(200).send('Link sent');
-                    })
-                ;
-            })
+            .post('https://api.pushover.net/1/messages.json')
+            .type('form')
+            .send(pushoverOptions)
         ;
-    }
+    })
+    .catch(err => {
+        if (err.response.status === 400) {
+            console.log(_.isObject(err.response.text))
+            if (_.isString(err.response.text) && JSON.parse(err.response.text).token === 'invalid') {
+                throw new ServerError('Unable to authenticate with Pushover.');
+            }
 
-    return res.status(400).send('Malformed request.');
+            throw new ServerError('Error sending to Pushover.\n' + JSON.stringify(JSON.parse(err.response.text), null, '  '));
+        }
+
+        return err;
+    })
+    .then(pushoverResponse => res.status(200).send('Link sent'))
+    .catch(ResponseError, error => res.status(error.status).send(error.message))
+    .catch(error => res.status(500).send(error.toString()))
+    ;
 });
 
-server = app.listen(process.env.PORT || 3000, function () {
-    var host = server.address().address;
-    var port = server.address().port;
+let server = app.listen(process.env.PORT || 3000, () => {
+    let host = server.address().address;
+    let port = server.address().port;
 
     console.log('Example app listening at http://%s:%s', host, port);
 });
